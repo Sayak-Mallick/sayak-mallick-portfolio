8388f6a052de66cde1bbae683941599c48a400d9
sayakmallick4875 <sayak@solivatech.com>
2025-09-04 17:32:55 +0530

    feat: remove welcome paragraph from App component and add styles file

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ function App() {
     <>
       <div>
         3D Portfolio Website
-        <p>
-          Welcome to my 3D portfolio website! Here you can find my latest projects and
-          designs created using cutting-edge 3D technologies.
-        </p>
       </div>
     </>
   )
