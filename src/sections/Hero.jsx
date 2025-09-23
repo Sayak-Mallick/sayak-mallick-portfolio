063bfbddabd2a01ed33b27ce186ad12b62c0385d
sayakmallick4875 <sayak@solivatech.com>
2025-09-23 14:43:58 +0530

    feat: enhance Hero section with GSAP animations, add AnimatedCounter and HeroExperience components, and update constants for navigation and counters

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import Button from '../components/Button';
+import HeroExperience from '../components/models/hero_models/HeroExperience';
+import { words } from "../constants";
+import React from "react";
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import AnimatedCounter from '../components/AnimatedCounter';
 
 export const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(".hero-text h1", { y: 50, opacity: 0 }, { y: 0, opacity: 1 , stagger: 0.2, duration: 1, ease: "power2.inOut"});
+  })
   return (
-    <div>Hero</div>
+    <section id="hero" className='relative overflow-hidden'>
+      <div className='absolute top-0 left-0 z-10'>
+        <img src="/images/bg.png" alt="background" />
+      </div>
+      <div className="hero-layout">
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word, index) => (
+                      <span
+                        key={index}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt="person"
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
+            </div>
+
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I’m Sayak Mallick, a developer based in Kolkata specializing in
+              crafting exceptional digital experiences. With a passion for
+              innovation and a commitment to excellence, I bring ideas to life
+              through code.
+            </p>
+
+            <Button text="Explore My Work" id="explore-button" className="md:w-80 md:h-16 w-60 h-12" />
+          </div>
+        </header>
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
+      </div>
+      <AnimatedCounter />
+    </section>
   )
 }
 
