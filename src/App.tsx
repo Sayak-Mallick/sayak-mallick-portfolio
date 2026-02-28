c05c469cf84ae0c5625af7b8cac1affcdf6e36c4
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-02-28 07:48:49 +0530

    Project Reinitialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
-import './App.css'
-import Navbar from "./components/Navbar.tsx";
-import Footer from "./components/Footer.tsx";
-import About from "./sections/About.tsx";
-import Home from "./sections/Home.tsx";
-import Skills from "./sections/Skills.tsx";
-import Projects from "./sections/Projects.tsx";
-import Experience from "./sections/Experience.tsx";
-import Testimonials from "./sections/Testimonials.tsx";
-import Contact from "./sections/Contact.tsx";
-import ParticlesBackground from "./components/ui/ParticlesBackground.tsx";
-import CustomCursor from "./components/ui/CustomCursor.tsx";
-import IntroAnimation from "./components/ui/IntroAnimation.tsx";
-import {useState} from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
-    const [introDone, setIntroDone] = useState(false);
-    return (
-        <>
-            {!introDone && <IntroAnimation onFinish={() => setIntroDone(true)} />}
-            <div className="relative gradient text-white scroll-smooth">
-                <CustomCursor/>
-                <ParticlesBackground/>
-                <Navbar/>
-                <Home/>
-                <About/>
-                <Skills/>
-                <Projects/>
-                <Experience/>
-                <Testimonials/>
-                <Contact/>
-                <Footer/>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Sayak Mallick</Button>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/sections/Skills.tsx
+++
@@ -1,8 +0,0 @@
-
-
-const Skills = () => {
-    return (
-        <div className="w-full h-screen">Skills</div>
-    )
-}
-export default Skills
