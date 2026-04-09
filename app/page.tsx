7c65be5f148cf27f0b3cb811b46e26b77a582503
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-04-09 09:43:32 +0530

    [TASK] : Portfolio initialized with Next js

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex">
+      <h1 className="text-3xl font-bold underline text-amber-300 dark:text-amber-500">Hello world!</h1>
+    </div>
+  );
+}
